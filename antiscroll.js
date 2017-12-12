6f580d6cabad2deddeaadc68dca3622ae310811e
Damian Krzeminski <pirxpilot@code42day.com>
2017-12-12 11:34:21 -0700

    use @pirxpilot/events

--- antiscroll.js
+++ antiscroll.js
@@ -1,48 +1,9 @@
 var css = require('css');
 var q = require('query');
 var inherit = require('inherit');
+var events = require('@pirxpilot/events');
 
 module.exports = Antiscroll;
-
-
-function events(el, obj) {
-  var handlers = {};
-
-  function bind(name, handler, opts) {
-    if (!handler) {
-      handler = name;
-    }
-    if (typeof(handler) === 'string') {
-      handler = obj[handler].bind(obj);
-    }
-    el.addEventListener(name, handler, opts);
-    handlers[name] = {
-      handler: handler,
-      opts: opts
-    };
-  }
-
-  function do_unbind(name) {
-    var h = handlers[name];
-    if (!h) { return; }
-    el.removeEventListener(name, h.handler, h.opts);
-    delete handlers[name];
-  }
-
-  function unbind(name) {
-    if (!name) { return unbindAll(); }
-    do_unbind(name);
-  }
-
-  function unbindAll() {
-    Object.keys(handlers).forEach(do_unbind);
-  }
-
-  return {
-    bind: bind,
-    unbind: unbind
-  };
-}
 
 /**
  * Antiscroll pane constructor.
